30ecd8a0d8f5f294a8f0c3ca231987deeef0424b
axvg <91773368+axvg@users.noreply.github.com>
2023-03-01 22:09:45 -0500

    Initial commit

---
+++ workshop-wordy-unscrambler/js/wordy.js
@@ -0,0 +1,54 @@
+import deePool from "./external/deePool.mjs";
+
+export default {
+  loadWords,
+  findWords
+};
+
+
+// ****************************
+
+var dict = new MinimalWordGraph()
+
+
+function loadWords(wordList) {
+  if (dict.size() > 0) {
+    dict = new MinimalWordGraph();
+  }
+
+  for (let word of wordList) {
+    dict.add(word);
+  }
+  dict.makeImmutable();
+
+  return dict.size();
+}
+
+function countLetters(str) {
+  var counts = {}
+  for (let i = 0; i < str.length; i++) {
+    counts[str[i]] = (counts[str[i]] || 0) + 1
+  }
+  return counts
+}
+
+function findWords(input) {
+  var inputCounts = countLetters(input)
+  var words = dict.containsOnly(
+    Array.isArray(input) ? input :
+      typeof input == "string" ? input.split("") :
+        []
+  )
+
+  words = [...(new Set(words))]
+
+  return words.filter(function removeWords(word) {
+    var wordLetterCounts = countLetters(word)
+    for (let [letter, count] of Object.entries(wordLetterCounts)) {
+      if (!inputCounts[letter] || count > inputCounts) {
+        return false
+      }
+    }
+    return true
+  })
+}
